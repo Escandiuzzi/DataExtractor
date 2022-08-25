ce98e4887e29c739a83f8e1bb2a8dcaa2b157cf9
Luiz Felipe Escandiuzzi <lfescandiuzzi@icloud.com>
2022-08-25 19:19:18 -0300

    Added PDF search

--- src/Managers/PDFManager.java
+++ src/Managers/PDFManager.java
@@ -1,13 +1,17 @@
 package Managers;
 
+import Utils.TextPositionSequence;
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.TextPosition;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PDFManager {
     private PDFParser parser;
@@ -40,6 +44,52 @@ public class PDFManager {
         pdfStripper.setEndPage(pdDoc.getNumberOfPages());
         Text = pdfStripper.getText(pdDoc);
         return Text;
+    }
+
+    List<TextPositionSequence> findSubwords(PDDocument document, int page, String searchTerm) throws IOException
+    {
+        final List<TextPositionSequence> hits = new ArrayList<TextPositionSequence>();
+        PDFTextStripper stripper = new PDFTextStripper()
+        {
+            @Override
+            protected void writeString(String text, List<TextPosition> textPositions) throws IOException
+            {
+                TextPositionSequence word = new TextPositionSequence(textPositions);
+                String string = word.toString();
+
+                int fromIndex = 0;
+                int index;
+                while ((index = string.indexOf(searchTerm, fromIndex)) > -1)
+                {
+                    hits.add(word.subSequence(index, index + searchTerm.length()));
+                    fromIndex = index + 1;
+                }
+                super.writeString(text, textPositions);
+            }
+        };
+
+        stripper.setSortByPosition(true);
+        stripper.setStartPage(page);
+        stripper.setEndPage(page);
+        stripper.getText(document);
+        return hits;
+    }
+
+
+    void printSubwords(PDDocument document, String searchTerm) throws IOException
+    {
+        System.out.printf("* Looking for '%s'\n", searchTerm);
+        for (int page = 1; page <= document.getNumberOfPages(); page++)
+        {
+            List<TextPositionSequence> hits = findSubwords(document, page, searchTerm);
+            for (TextPositionSequence hit : hits)
+            {
+                TextPosition lastPosition = hit.textPositionAt(hit.length() - 1);
+                System.out.printf("  Page %s at x: %s, y: %s with width %s and last letter '%s' at x: %s, y: %s\n",
+                        page, hit.getX(), hit.getY(), hit.getWidth(),
+                        lastPosition.getUnicode(), lastPosition.getXDirAdj(), lastPosition.getYDirAdj());
+            }
+        }
     }
 
     public void setFilePath(String filePath) {
