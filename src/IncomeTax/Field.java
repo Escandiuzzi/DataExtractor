2035214ce3c692bea69c7735d399bea7a378413e
claudia <clauu013@gmail.com>
2022-10-02 17:47:19 -0300

    Code refactoring

--- src/IncomeTax/Field.java
+++ src/IncomeTax/Field.java
@@ -1,5 +1,9 @@
 package IncomeTax;
 
+import utils.TextPositionSequence;
+
+import java.io.IOException;
+
 public class Field {
 
     public String name;
@@ -11,4 +15,64 @@ public class Field {
     public int height;
     public int width;
     public boolean table;
+
+    public void getContentField(Section section, ExtractData document, ExtractData.Result posSection, ExtractData.Result nextPosSection){
+
+        try {
+
+            ExtractData.Result campoResult = document.getSubwords(this.name, posSection.getPage(), (int)posSection.getPosition().getY(), nextPosSection == null ? 0 : (int)nextPosSection.getPosition().getY(), nextPosSection == null ? posSection.getPage() + 1 :nextPosSection.getPage());
+
+            if (campoResult == null){
+                System.out.println("Não encontrou posicao do termo " + this.name + " page: " + posSection.getPage());
+                return;
+            }
+
+            TextPositionSequence position = campoResult.getPosition();
+
+            if (this.table){
+                this.content = document.getTextByArea((int)position.getX() - 25, (int)position.getY() + (this.y > 0 ? this.y : this.height) , (int)position.getWidth(), this.height, this, campoResult.getPage() - 1);
+            } else {
+                this.content = document.getTextByArea((int)position.getX() + (int)position.getWidth() + 10, (int)position.getY(), (int)position.getWidth() + 200, this.height, this, campoResult.getPage() - 1);
+            }
+
+            System.out.println( "Field: " + this.name + ": " + this.content);
+
+            if (section.table){
+
+                String content2 = "a";
+
+                int y = (int) position.getY() + section.numberOfLines;
+
+                System.out.println("Section is table !!!");
+
+                while (!content2.trim().isEmpty()) {
+
+                    if (nextPosSection != null){
+                        if (nextPosSection.getPosition().getY() < y && nextPosSection.getPage() == posSection.getPage() ){
+                            break;
+                        }
+                    }
+
+                    y += section.numberOfLines;
+
+                    System.out.println("Position Get Y " + y);
+
+                    if (this.table) {
+                        content2 = document.getTextByArea((int) position.getX() - 25, y, (int) position.getWidth(), this.height, this, campoResult.getPage() - 1);
+                    } else {
+                        content2 = document.getTextByArea((int) position.getX() + (int) position.getWidth() + 10, y, (int) position.getWidth() + 200, this.height, this, campoResult.getPage() - 1);
+                    }
+
+                    if (!content2.trim().isEmpty()){
+                        System.out.println("Field 2: " + this.name + ": " + content2);
+                    }
+
+                }
+
+            }
+
+        } catch (IOException dfg) {
+            System.out.println(dfg);
+        }
+    }
 }
--- src/IncomeTax/GetIncomeTax.java
+++ src/IncomeTax/GetIncomeTax.java
@@ -1,7 +1,5 @@
 package IncomeTax;
 
-import utils.TextPositionSequence;
-
 import java.io.IOException;
 
 public class GetIncomeTax {
@@ -26,112 +24,43 @@ public class GetIncomeTax {
 
         ExtractData document = new ExtractData();
 
+        if (incomeTax == null) {
+            return;
+        }
+
         try {
             document.openDocument("C:\\imposto.pdf");
 
-            if (incomeTax != null) {
-                for (int i = 0; i<incomeTax.sections.length; i++){
+            for (int i = 0; i<incomeTax.sections.length; i++){
 
-                    Section section = incomeTax.sections[i];
+                Section section = incomeTax.sections[i];
 
-                    try {
-                        ExtractData.Result nextResult = null;
-                        ExtractData.Result result = document.getSubwords(section.name, 0, 0, 5000, 0);
+                try {
+                    ExtractData.Result nextResult = null;
+                    ExtractData.Result result = document.getSubwords(section.name, 0, 0, 5000, 0);
 
-                        if (result == null){
-                            System.out.println("Não encontrou posicao do termo " + section.name);
-                            continue;
-                        }
-
-                        if ((i + 1) < incomeTax.sections.length){
-                            nextResult = document.getSubwords(incomeTax.sections[i+1].name, 0, 0, 5000, 0);
-
-                            /*
-                                if (nextResult == null){
-                                    continue;
-                                }
-
-                             */
-
-                        }
-
-                        System.out.println("RESULT: " + section.name + " X: " + result.getPosition().getX() + " Y: " + result.getPosition().getY() + " PAGE: " + result.getPage());
-
-                        for (int j = 0; j< section.fields.length; j++){
-
-                            Field field = section.fields[j];
-
-                            try {
-
-                                ExtractData.Result campoResult = document.getSubwords(field.name, result.getPage(), (int)result.getPosition().getY(), nextResult == null ? 0 : (int)nextResult.getPosition().getY(), nextResult == null ? result.getPage() + 1 :nextResult.getPage());
-
-                                if (campoResult == null){
-                                    System.out.println("Não encontrou posicao do termo " + field.name + " page: " + result.getPage());
-                                    continue;
-                                }
-
-                                TextPositionSequence position = campoResult.getPosition();
-
-                                String content = "";
-
-                                if (field.table){
-                                    content = document.getTextByArea((int)position.getX() - 25, (int)position.getY() + (field.y > 0 ? field.y : field.height) , (int)position.getWidth(), field.height, field, campoResult.getPage() - 1);
-                                } else {
-                                    content = document.getTextByArea((int)position.getX() + (int)position.getWidth() + 10, (int)position.getY(), (int)position.getWidth() + 200, field.height, field, campoResult.getPage() - 1);
-                                }
-
-                                System.out.println( "Field: " + field.name + ": " + content);
-
-                                if (section.table){
-
-                                    String content2 = "a";
-
-                                    int y = (int) position.getY() + section.numberOfLines;
-
-                                    System.out.println("Section is table !!!");
-
-                                    while (!content2.trim().isEmpty()) {
-
-                                        if (nextResult != null){
-                                            if (nextResult.getPosition().getY() < y && nextResult.getPage() == result.getPage() ){
-                                                break;
-                                            }
-                                        }
-                                        content2 = "";
-
-                                        y += section.numberOfLines;
-
-                                        System.out.println("Position Get Y " + y);
-
-                                        if (field.table) {
-                                            content2 = document.getTextByArea((int) position.getX() - 25, y, (int) position.getWidth(), field.height, field, campoResult.getPage() - 1);
-                                        } else {
-                                            content2 = document.getTextByArea((int) position.getX() + (int) position.getWidth() + 10, y, (int) position.getWidth() + 200, field.height, field, campoResult.getPage() - 1);
-                                        }
-
-                                        if (!content2.trim().isEmpty()){
-                                            System.out.println("Field 2: " + field.name + ": " + content2);
-                                        }
-
-                                    }
-
-                                }
-
-                                // System.out.println("RESULT CAMPO: " + field.name + " X: " + position.getX() + " Y: " + position.getY() + " PAGE: " + campoResult.getPage());
-
-
-                            } catch (IOException dfg) {
-                                System.out.println(dfg);
-                            }
-
-                        }
-
-
-                    } catch (IOException dfg) {
-                        System.out.println(dfg);
+                    if (result == null){
+                        System.out.println("Não encontrou posicao do termo " + section.name);
+                        continue;
                     }
 
+                    if ((i + 1) < incomeTax.sections.length){
+                        nextResult = document.getSubwords(incomeTax.sections[i+1].name, 0, 0, 5000, 0);
+                    }
+
+                    System.out.println("RESULT: " + section.name + " X: " + result.getPosition().getX() + " Y: " + result.getPosition().getY() + " PAGE: " + result.getPage());
+
+                    for (int j = 0; j< section.fields.length; j++){
+
+                        section.fields[j].getContentField(section, document, result, nextResult);
+
+                    }
+
+
+                } catch (IOException dfg) {
+                    System.out.println(dfg);
                 }
+
             }
 
         } catch (IOException error) {
